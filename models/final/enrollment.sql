459b0485bac3a0c6bf020dd3894c8dfa7d0fbf97
Mike Krahulec <mike@z2healthinsights.com>
2026-02-03 21:40:19 -0600

    Add model definitions

--- models/final/enrollment.sql
+++ models/final/enrollment.sql
@@ -1,7 +1,7 @@
 SELECT
   BENE_MBI_ID as current_bene_mbi_id,
-  period_start_date as enrollment_start_date, -- @TODO - Refactor, period start date may be before start of contiguous enrollment period
-  period_end_date as enrollment_end_date, -- @TODO - Refactor, period end date may be after start of contiguous enrollment period
+  period_start_date as enrollment_start_date,
+  period_end_date as enrollment_end_date,
   enroll_month as bene_member_month,
   file_name,
   period_end_date as file_date
