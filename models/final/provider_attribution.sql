c3b6900b2b54262bf7146484cbb76c57140023db
Mike Krahulec <mike@z2healthinsights.com>
2026-04-09 09:36:24 -0500

    Update payer and plan to match CCLF connector

--- models/final/provider_attribution.sql
+++ models/final/provider_attribution.sql
@@ -2,8 +2,8 @@ SELECT
     cast(bene_mbi_id as {{ dbt.type_string() }}) as person_id
   , cast(bene_mbi_id as {{ dbt.type_string() }}) as patient_id
   , cast({{ format_yyyymm('enroll_month') }} as {{ dbt.type_string() }}) as year_month
-  , cast('Medicare' as {{ dbt.type_string() }}) as payer
-  , cast(null as {{ dbt.type_string() }}) as {{ quote_column('plan') }}
+  , cast('medicare' as {{ dbt.type_string() }}) as payer
+  , cast('medicare' as {{ dbt.type_string() }}) as {{ quote_column('plan') }}
   , cast('medicare' as {{ dbt.type_string() }}) as data_source
   , cast(top_npi as {{ dbt.type_string() }}) as payer_attributed_provider
   , cast(top_tin as {{ dbt.type_string() }}) as payer_attributed_provider_practice
